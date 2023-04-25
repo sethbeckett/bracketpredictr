b245c57b122aaec23fa130e6ada804ef382833f6
chardyb <chardy723@icloud.com>
2023-04-25 10:34:15 -0600

    adding validation function

--- R/validate_inputs.R
+++ R/validate_inputs.R
@@ -1,3 +1,16 @@
 validate_inputs <- function(teams, stats, years) {
-
+  if (!all(teams %in% cbb$TEAMS)){
+   stop("The team you entered is not in the data, call 'get_team_names()' to find the team you are looking for")
+  }
+  #stats
+  if (!all(stats %in% names(cbb))){
+    stop("This is a not a stat we have for basketball")
+  }
+  #years
+  if (!is.integer(years)){
+    stop("enter in valid year")
+  }
+  if (2013 <= years && years <= 2019) {
+      return(TRUE)
+  }
 }
