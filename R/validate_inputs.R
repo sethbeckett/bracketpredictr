bdf0aad7c51e8a4df419801474ce4e8af74028e5
chardyb <chardy723@icloud.com>
2023-04-25 10:54:15 -0600

    fixed

--- R/validate_inputs.R
+++ R/validate_inputs.R
@@ -1,6 +1,18 @@
+#' Validate inputs
+#'
+#' @param teams The college basketball team you want to find the stats of
+#' @param stats Different statistics provided about the basketball team entered
+#' @param years The year that corrisponds with the season you want to know the
+#' stats of for the specific team
+#'
+#' @return If the validate function is called and its not valid it will return
+#' a stop message
+#' @export
+#'
 validate_inputs <- function(teams, stats, years) {
-  if (!all(teams %in% cbb$TEAMS)){
-   stop("The team you entered is not in the data, call 'get_team_names()' to find the team you are looking for")
+  if (!all(teams %in% cbb$TEAM)){
+   stop("The team you entered is not in the data, call 'get_team_names()' to
+        find the team you are looking for")
   }
   #stats
   if (!all(stats %in% names(cbb))){
